122a7db75fbdf82f752462fd0ab48d084d93a1c2
Bortl Jan <jan.bortl@nordictelecom.cz>
2023-11-20 18:05:51 +0100

    config example

---
+++ config.example.pl
@@ -0,0 +1,11 @@
+$_CONFIG{fcgi_socket} = 'localhost:18088';
+$_CONFIG{fcgi_queue} = 10;
+
+$_CONFIG{db_type} = 'mysql';
+$_CONFIG{db_database} = 'treemanager';
+$_CONFIG{db_host} = 'localhost';
+$_CONFIG{db_user} = 'treemanager';
+$_CONFIG{db_pass} = '<insert_some_fancy_password>';
+
+1;
+
